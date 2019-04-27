f73b753bd3585c549bcae8826b98103bbee49749
sriley <sarahriley.dev@gmail.com>
2019-04-26 21:54:26 -0400

    first commit

---
+++ guide-post/src/data.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    id: 1,
+    name: "Jim",
+    location: "",
+    duration: "",
+    rating: 0
+  },
+  {
+    id: 2,
+    name: "Sarah",
+    location: "",
+    duration: "",
+    rating: 0
+  },
+  {
+    id: 3,
+    name: "Bella",
+    location: "",
+    duration: "",
+    rating: 0
+  },
+  {
+    id: 4,
+    name: "James",
+    location: "",
+    duration: "",
+    rating: 0
+  },
+  {
+    id: 5,
+    name: "Kaitlin",
+    location: "",
+    duration: "",
+    rating: 0
+  }
+];
+
+export default data;
